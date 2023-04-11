fe1d786d579c480e9a7a70419b7eb65a1b8749ba
Bat-lab <batmail555@protonmail.com>
2023-04-11 17:03:08 +0530

    ubuntu

--- scripts/install/one-more.sh
+++ scripts/install/one-more.sh
@@ -19,11 +19,11 @@ echo "$password"
 
       wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-      dpkg -i ~/google-chrome-stable_current_amd64.deb
+      dpkg -i ./google-chrome-stable_current_amd64.deb
 
       wget https://download.anydesk.com/linux/anydesk_6.2.1-1_amd64.deb
 
-      dpkg -i ~/anydesk_6.2.1-1_amd64.deb
+      dpkg -i ./anydesk_6.2.1-1_amd64.deb
       apt --fix-broken install -y
 
       apt install openjdk-8-jdk -y
